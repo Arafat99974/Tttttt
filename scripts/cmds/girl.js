691e0dc45c1353ad7812c8e0bfc1f9e7bde8c766
S.i RaNa <121483619+sirana2522@users.noreply.github.com>
2025-02-17 20:00:10 +0600

    Update girl.js

--- scripts/cmds/girl.js
+++ scripts/cmds/girl.js
@@ -5,7 +5,7 @@ module.exports = {
 		version: "1.0",
 		author: "ShAn",
 		countDown: 5,
-		role: 2,
+		role: 0,
 		shortDescription: "send a girl photos",
 		longDescription: "send a sexy girl photos",
 		category: "18+",
