c302022e709d810e1c57c74529f8a542f7571223
S.i RaNa <121483619+sirana2522@users.noreply.github.com>
2025-02-17 19:58:41 +0600

    Update n.js

--- scripts/cmds/n.js
+++ scripts/cmds/n.js
@@ -5,7 +5,7 @@ module.exports = {
 		version: "1.0",
 		author: "ShAn",
 		countDown: 5,
-		role: 2,
+		role: 0,
 		shortDescription: "send a pic of nude",
 		longDescription: "sends a pic of girls nude",
 		category: "18+",
