9f10e3a5c189ce3bcdfcb276b51e4f7d8058d1a8
S.i RaNa <121483619+sirana2522@users.noreply.github.com>
2025-03-21 16:27:48 +0600

    Update autodl.js

--- scripts/cmds/autodl.js
+++ scripts/cmds/autodl.js
@@ -32,7 +32,7 @@ const platforms = {
   },
   YouTube: {
     regex: /(?:https?:\/\/)?(?:www\.)?(youtube\.com|youtu\.be)/,
-    endpoint: "/nazrul/ytDL?url=",
+    endpoint: "/nazrul/ytDL2?uri=",
   },
   Twitter: {
     regex: /(?:https?:\/\/)?(?:www\.)?x\.com/,
