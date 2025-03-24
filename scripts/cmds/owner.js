f11fb514b47dcd6ddeaeec6d9c51f2dc01ffa5d0
S.i RaNa <121483619+sirana2522@users.noreply.github.com>
2025-03-24 10:18:52 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -1,6 +1,5 @@
 const { GoatWrapper } = require('fca-liane-utils');
 const axios = require('axios');
-const fs = require('fs');
 const path = require('path');
 const moment = require('moment-timezone');
 
@@ -48,11 +47,26 @@ config: {
     const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
     const ShAn = [
-      'https://drive.google.com/uc?export=download&id=15PMxc-gznm4Ona3fZLPTs7Yj3kOfx0Wf',
-      'https://drive.google.com/uc?export=download&id=17Udy1YF9M0f6kVkTY_I2s_Lgy3iXheuj',
-      'https://drive.google.com/uc?export=download&id=12PMfrf5d0ahx0e2fvfN4XB3G1hi7SusZ',
-      'https://drive.google.com/uc?export=download&id=12BOEV1OOgz464trZMHeoWv9YkW9L2TAj',
-      'https://drive.google.com/uc?export=download&id=127TkJBb_JGlQAnQZx-YUnPdrS44Zqbit'
+'https://drive.google.com/uc?export=download&id=1J4yQ13L2WTpdOuqcP0yEmzULACdwfvnQ',
+'https://drive.google.com/uc?export=download&id=1J2ph0PcizaIy9QnK9790j4jXGBwBSjG7',
+'https://drive.google.com/uc?export=download&id=1IxpwGICHS9rt7UkwYbkTO4PlvMn5jG97',
+'https://drive.google.com/uc?export=download&id=1Ix3Dumgwq0CieVtSpYhrpR9d32WXrmiV',
+'https://drive.google.com/uc?export=download&id=1IqXSVvZtP3a34-sJk_quBTaJz0yxEmr0',
+'https://drive.google.com/uc?export=download&id=1IeFbLyT10lgmoFGusHG7iiYennklu8AG',
+'https://drive.google.com/uc?export=download&id=1IdK2aYg3Ghea2-GZCEIbGfM0n3pvybCW',
+'https://drive.google.com/uc?export=download&id=1IWXtQBd9V5xuLL7-dADTKLoprAE6XrOS',
+'https://drive.google.com/uc?export=download&id=1IOW6p0wpC6E8MBv1OfAnG1UWmMKfPw5z',
+'https://drive.google.com/uc?export=download&id=1IKA_yxFxPz09116TSmrk-K55DEW3GFB2',
+'https://drive.google.com/uc?export=download&id=1IH8zjj-fH5M8hwPrI1i6JfKFRlmy7AyX',
+'https://drive.google.com/uc?export=download&id=1IEz8wwc5T5chRzsruT8fETfqJUYTk_1g',
+'https://drive.google.com/uc?export=download&id=1I5iOweiVL_aV8W2k3WJqgEHkHD75PY1k',
+'https://drive.google.com/uc?export=download&id=1I3Jte-iy8bF4SehZ_EHN-EOeoeJSscEO',
+'https://drive.google.com/uc?export=download&id=1I1vfvQnpx6OW9iyM55OGAerAge19bUP4',
+'https://drive.google.com/uc?export=download&id=1I0YRd6OzpRHLFM-pqYmoKuDRe9Ldhfht',
+'https://drive.google.com/uc?export=download&id=1HyhkBI92QKm3dTq6NJpDhGWKHFKn8iN8',
+'https://drive.google.com/uc?export=download&id=1Hy9SmvIJzU5aXbjjGTlEUwCihOWIj6Fk',
+'https://drive.google.com/uc?export=download&id=1Hw-0A--3teOH5k9zWsAc85gwGbxgIJJR',
+'https://drive.google.com/uc?export=download&id=1HqWwyPkSHp7G_HgcH5bUNOW4nQrlcUFy'
       ]; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
     const ShaN = ShAn[Math.floor(Math.random() * ShAn.length)];
 
