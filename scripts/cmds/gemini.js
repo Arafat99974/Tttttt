302c2ac0d370e3cf0b7b30a21cecaf7dc6563b59
S.i RaNa <121483619+sirana2522@users.noreply.github.com>
2025-03-29 20:17:15 +0600

    Update gemini.js

--- scripts/cmds/gemini.js
+++ scripts/cmds/gemini.js
@@ -1,95 +1,63 @@
 const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
 
 module.exports = {
-	config: {
-		name: "gemini",
-		version: "1.0",
-		author: "𝗦𝗵𝗔𝗻",
-		description:"gemeini ai",
-		countDown: 5,
-		role: 0,
-		category: "𝗔𝗜",
-		guide: {
-			en: "{pn} message | photo reply"
-		}
-	},
-	onStart:async ({ api, args , event}) => {
-  const prompt = args.join(' ');
-  const apis =
-["AIzaSyB0dAHTjS-1J7gG1om1GszmiHN8z2rlP20",
-"AIzaSyAY9gYYYLVKEo5CdzLGE-qMwLc9sFiNYAI",
-"AIzaSyDho4C6AH4hTVkmxSrrLYiH1XduArD42Ko",
-"AIzaSyBgqgnSY2wizm3CJPe52kz_HFFfdFoMyuw",
-"AIzaSyCpxY-PLdWnPlchnRalQgFwmxJ9G4blaFg",
-"AIzaSyA1dXP05uWo6IOM8rays8pvVAXImWIjxgM"]
-//,"AIzaSyDDUItpo6erFPebw_JBu9kMw8H4gNQk31A"];
-  const apiKey = apis[Math.floor(Math.random() * apis.length)]
-//---- Image Reply -----//
-   if (event.type === "message_reply") {
+  config: {
+    name: "gemini",
+    aliases: ["gmi"],
+    version: "1.0",
+    author: "𝗦𝗵𝗔𝗻",
+    description: "gemeini ai",
+    countDown: 5,
+    role: 0,
+    category: "𝗔𝗜",
+    guide: {
+      en: "{pn} message | photo reply",
+    },
+  },
+  onStart: async ({ api, args, event }) => {
+    const prompt = args.join(" ");
+    //---- Image Reply -----//
+    if (event.type === "message_reply") {
       var t = event.messageReply.attachments[0].url;
-    const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=${apiKey}`;
-    try {
-      const response = await axios.get(t, { responseType: "arraybuffer" });
-  const buffer = Buffer.from(response.data, "binary");
-    const requestBody = {
-      contents: [
-        {
-          parts: [
-            { text: prompt },
-            {
-              inline_data: {
-                mime_type: 'image/jpeg',
-                data: buffer.toString("base64"),
-              }
-            }
-          ]
-        }
-      ]
-    };
-    const generationResponse = await axios.post(apiUrl, requestBody, {
-      headers: {
-        'Content-Type': 'application/json'
+      try {
+        const response = await axios.get(
+          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}&url=${encodeURIComponent(t)}`,
+        );
+        const data2 = response.data.dipto;
+        api.sendMessage(data2, event.threadID, event.messageID);
+      } catch (error) {
+        console.error("Error:", error.message);
+        api.sendMessage(error, event.threadID, event.messageID);
       }
-    });
-    const gText = generationResponse.data.candidates[0].content;
-const data2 = gText.parts[0].text;// || 'No text found in the response';
-    console.log('Generated Text:', data2);
-  api.sendMessage(data2, event.threadID, event.messageID);
-  } catch (error) {
-    console.error('Error:', error.message);
-    api.sendMessage(error, event.threadID, event.messageID);
-  }
-  }
-  //---------- Message Reply ---------//
-else if(!prompt) {
-   return api.sendMessage('Please provide a prompt or message reply', event.threadID, event.messageID);}
-    else {
-  try {
-    const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`;
-    const prompt1 = prompt+' Give answer in Bangla';
-  const data = {
-    'contents': [
-      {
-        'parts': [
-          {
-            'text': prompt1
-          }
-        ]
+    }
+    //---------- Message Reply ---------//
+    else if (!prompt) {
+      return api.sendMessage(
+        "Please provide a prompt or message reply",
+        event.threadID,
+        event.messageID,
+      );
+    } else {
+      try {
+        const respons = await axios.get(
+          `${await baseApiUrl()}/gemini?prompt=${encodeURIComponent(prompt)}`,
+        );
+        const message = respons.data.dipto;
+        api.sendMessage(message, event.threadID, event.messageID);
+      } catch (error) {
+        console.error("Error calling Gemini AI:", error);
+        api.sendMessage(
+          `Sorry, there was an error processing your request.${error}`,
+          event.threadID,
+          event.messageID,
+        );
       }
-    ]
-  };
-  const headers = {
-    'Content-Type': 'application/json'
-  };
-    const response = await axios.post(apiUrl , data, { headers });
-    const data1 = response.data.candidates[0].content;
-    const message = data1.parts[0].text;
-  console.log(prompt1);
-    api.sendMessage(message, event.threadID,event.messageID);
-    } catch (error) {
-      console.error('Error calling Gemini AI:', error);
-      api.sendMessage(`Sorry, there was an error processing your request.${error}`, event.threadID, event.messageID);
-  }
- }
-}
+    }
+  },
 };
